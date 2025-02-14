d7269b75d831d5bc06e0b6ab1551ccf61e003c74
Rokan3583 <joskiytelik@gmail.com>
2025-02-15 01:35:35 +0300

    new work

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # PG_Alexsandrov
- 
--- каркас.py
+++ каркас.py
@@ -1,18 +1,21 @@
-import pygame as pg
-pg.init()
-size = (1280, 720)
-screen = pg.display.set_mode(size)
-pg.display.set_caption("Моя Игра")
-BACKGROUND = (255, 255, 255)
+import pygame
+pygame.init()
+
+size = (1200, 720)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("новая игра")
+BACKGROUND = (255,255,255)
 screen.fill(BACKGROUND)
 FPS = 60
-clock = pg.time.Clock()
+clock = pygame.time.Clock()
 running = True
 while running:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             running = False
+
     screen.fill(BACKGROUND)
-    pg.display.flip()
+    pygame.display.flip()
     clock.tick(FPS)
-pg.quit()
+
+pygame.quit()
